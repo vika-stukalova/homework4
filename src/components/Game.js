23bec3cb71301ae99b672060c6efbeb3109a2e59
vika-stukalova <90108074+vika-stukalova@users.noreply.github.com>
2022-03-23 15:52:17 -0700

    Initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles/Game.css';
+import Board from "./Board";
+
+function Game() {
+    
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+        </div>
+    )
+}
+
+export default Game;
